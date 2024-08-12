3b54db640a07f163ebd9a6030b8798dadd611412
rakshil14-2 <rhmodi@asu.edu>
2024-08-12 13:16:20 -0700

    Adding Tailwind configurations

--- src/main/java/com/scm/controllers/PageController.java
+++ src/main/java/com/scm/controllers/PageController.java
@@ -2,6 +2,7 @@ package com.scm.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -13,5 +14,15 @@ public class PageController {
         mode.addAttribute("name","Rakshil Modi" );
         mode.addAttribute("Springboot Project", "Smart Contact Manager");
         return "Home";
+    }
+
+    @GetMapping("/about")
+    public String aboutPage(){
+        return "about";
+    }
+
+    @GetMapping("/services")
+    public String services(){
+        return "services";
     }
 }
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/main/resources/**/*.{html,js}"], // Like whatever file in resources with html and js extesnion must have talwindcss
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  darkMode:"selector",
+}
+
